a25b6874de12fe27e1545dda79370a5792ab047e
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2023-06-24 17:52:00 +0900

    working act installer

--- installers/_core.bash
+++ installers/_core.bash
@@ -36,8 +36,10 @@ fi
 
 script_path="$(readlink -e -- "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")"
 script_name="$(basename "${script_path}")"
+script_dir="$(dirname "${script_path}")"
 info "SP:${script_path}"
 info "SN:${script_name}"
+info "SD:${script_dir}"
 IFS=$'.' read -r install_name install_type <<< "${script_name%.bash}"
 info "IN:${install_name}"
 info "IT:${install_type}"
@@ -47,21 +49,44 @@ set -o nounset
 set -o pipefail
 IFS=$'\n\t'
 
-PID=$$
+if [[ "$(id -u)" -eq "0" ]]; then
+  fail "please DO NOT run as root";
+  return 1
+fi
+
+pid=$$
+workdir="/tmp/${install_name}"
 install_log="/tmp/${install_name}.log"
+
+# directory that keeps track of successful installs
+cache_dir="${script_dir}/cache"
+mkdir -p "${cache_dir}"
+done="${cache_dir}/${install_name}.done"
+
+# if app was successfully installed it is skipped on re-runs
+if [[ "${1:-}" == "--force" ]]; then
+  shift
+elif [[ -f "${done}" ]]; then
+  echo ":: ${install_name}:${pid} skip"
+  exit 0
+fi
+
+# if we are re-running, delete the previous install working dir
+[[ -d "${workdir}" ]] && rm -rf "${workdir}"
 
 # log stdout/stderr to a file and stdout
 exec &> >(tee "${install_log}")
 
 function cleanup {
-  # shellcheck disable=SC2154
   if [ -n "${1:-}" ]; then
-    echo ":: ${install_name}:${PID} Aborted by ${1:-}"
+    echo ":: ${install_name}:${pid} Aborted by ${1:-}"
   elif [ "${status}" -ne 0 ]; then
-    echo ":: ${install_name}:${PID} Failure (status $status)"
+    echo ":: ${install_name}:${pid} Failure (status $status)"
   else
-    echo ":: ${install_name}:${PID} Success"
+    echo ":: ${install_name}:${pid} Success"
+    touch "${done}"
   fi
+  popd > /dev/null
 }
 export -f cleanup
 trap 'status=$?; cleanup; exit $status' EXIT
@@ -69,136 +94,5 @@ trap 'trap - HUP; cleanup SIGHUP; kill -HUP $$' HUP
 trap 'trap - INT; cleanup SIGINT; kill -INT $$' INT
 trap 'trap - TERM; cleanup SIGTERM; kill -TERM $$' TERM
 
-return 0
-
-
-
-
-  script_name="$(basename "${CURRENT_SCRIPT}")"
-  SCRIPT_DIR="${CURRENT_SCRIPT_DIR}"
-  SCRIPT_FULL_PATH="${CURRENT_SCRIPT_DIR}/${script_name}"
-
-  set +o allexport
-
-  [[ -n "${script_name}" ]] && bash "${SCRIPT_FULL_PATH}" $@
-fi
-#!/usr/bin/env bash
-
-# full path to parent script
-SCRIPT="$(readlink -e -- "${0}")"
-# parent script file name
-SCRIPT_BASE="$(basename "${SCRIPT}")"
-
-# full path to this file
-SOURCE_FILE="$(readlink -e -- "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")"
-# directory of this file
-SOURCE_DIR="$(dirname "${SOURCE_FILE}")"
-
-if [[ "${SCRIPT_BASE}" == 'core.source' ]]; then
-  echo ":: cannot call core.source directly"
-  exit 1
-fi
-
-if [[ "$(id -u)" -eq "0" ]]; then
-  echo "please DO NOT run as root";
-  exit 1
-fi
-
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -o errexit
-set -o nounset
-set -o pipefail
-IFS=$'\n\t'
-
-PID=$$
-LOG="/tmp/${SCRIPT_BASE}.log"
-
-# log stdout/stderr to a file and stdout
-exec &> >(tee "${LOG}")
-
-function cleanup {
-  # shellcheck disable=SC2154
-  if [ -n "${1:-}" ]; then
-    echo ":: ${SCRIPT_BASE}:${PID} Aborted by ${1:-}"
-  elif [ "${status}" -ne 0 ]; then
-    echo ":: ${SCRIPT_BASE}:${PID} Failure (status $status)"
-  else
-    echo ":: ${SCRIPT_BASE}:${PID} Success"
-  fi
-}
-export -f cleanup
-trap 'status=$?; cleanup; exit $status' EXIT
-trap 'trap - HUP; cleanup SIGHUP; kill -HUP $$' HUP
-trap 'trap - INT; cleanup SIGINT; kill -INT $$' INT
-trap 'trap - TERM; cleanup SIGTERM; kill -TERM $$' TERM
-#!/usr/bin/env /usr/local/bin/env-ubuntu-core
-# NOTE: OSX requires shebang to be binary file not script, this is the workaround
-# shellcheck disable=SC1090
-. "${SCRIPT_DIR}/core.source"
-
-REPO="glg/gdsservice"
-
-# Only change the above
-
-kernel_name="$(uname -s)" # Darwin/Linux
-machine="$(uname -m)"
-if [[ "${machine}" == "x86_64" ]]; then
-  architecture="amd64"
-else
-  architecture="arm64"
-fi
-
-# NOTE: sometimes you need ${kernel_name,,}
-read -r VERSION URL <<< "$(gh api "/repos/${REPO}/releases/latest" \
-  | jq \
-  --arg k "${kernel_name}" \
-  --arg a "${architecture}" \
-  --arg m "${machine}" \
-  -r '[.tag_name, (.assets[].browser_download_url | select(test($k+"_"+$m)))] | @tsv' \
-)"
-
-ZIP_FILE="${URL##*/}"
-EXECUTABLE="${REPO##*/}"
-VERSIONED_EXE="${EXECUTABLE}_${VERSION}"
-
-gh release download "${VERSION}" -R "${REPO}" -p "${ZIP_FILE}"
-
-TARGET_DIR="${HOME}/.local/${EXECUTABLE}"
-BIN_DIR="${HOME}/.local/bin"
-
-mkdir -p "${TARGET_DIR}"
-mkdir -p "${BIN_DIR}"
-
-tar -xf "${ZIP_FILE}"
-#unzip "${ZIP_FILE}"
-
-mv "${EXECUTABLE}" "${TARGET_DIR}/${VERSIONED_EXE}"
-
-if [[ "${kernel_name}" == "Darwin" ]]; then
-  ln -sf "${TARGET_DIR}/${VERSIONED_EXE}" "${BIN_DIR}/${EXECUTABLE}"
-else
-  ln --symbolic --force "${TARGET_DIR}/${VERSIONED_EXE}" "${BIN_DIR}/${EXECUTABLE}"
-fi
-
-# vim: set ft=bash :
-#!/usr/bin/env /usr/local/bin/env-ubuntu-core
-# NOTE: OSX requires shebang to be binary file not script, this is the workaround
-# shellcheck disable=SC1090
-. "${SCRIPT_DIR}/core.source"
-
-VERSION="${1:-0.2.42}"
-TARGET="/usr/local/bin/act"
-KERNEL="$(uname -s)"
-HARDWARE="$(uname -m)"
-
-if [[ "${KERNEL}" == "Linux" ]]; then
-  architecture="$(dpkg --print-architecture)"
-else
-  >&2 echo ":: unsupported kernel"
-fi
-
-wget -q "https://github.com/nektos/act/releases/download/v${VERSION}/act_${KERNEL}_${HARDWARE}.tar.gz"
-tar xzf act_${KERNEL}_${HARDWARE}.tar.gz
-sudo mv "act" "${TARGET}"
-
-# vi: set ft=bash :
+mkdir -p "${workdir}"
+pushd "${workdir}" > /dev/null
--- installers/act.manual.bash
+++ installers/act.manual.bash
@@ -1,127 +1,58 @@
 #!/usr/bin/env bash
-
 source './_core.bash'
 
-bla
+### MODIFY: START
+repo="nektos/act"
+### MODIFY: END
 
-exit 0
-
-
-# full path to parent script
-SCRIPT="$(readlink -e -- "${0}")"
-# parent script file name
-SCRIPT_BASE="$(basename "${SCRIPT}")"
-
-# full path to this file
-SOURCE_FILE="$(readlink -e -- "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")"
-# directory of this file
-SOURCE_DIR="$(dirname "${SOURCE_FILE}")"
-
-if [[ "${SCRIPT_BASE}" == 'core.source' ]]; then
-  echo ":: cannot call core.source directly"
-  exit 1
-fi
-
-if [[ "$(id -u)" -eq "0" ]]; then
-  echo "please DO NOT run as root";
-  exit 1
-fi
-
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -o errexit
-set -o nounset
-set -o pipefail
-IFS=$'\n\t'
-
-PID=$$
-LOG="/tmp/${SCRIPT_BASE}.log"
-
-# log stdout/stderr to a file and stdout
-exec &> >(tee "${LOG}")
-
-function cleanup {
-  # shellcheck disable=SC2154
-  if [ -n "${1:-}" ]; then
-    echo ":: ${SCRIPT_BASE}:${PID} Aborted by ${1:-}"
-  elif [ "${status}" -ne 0 ]; then
-    echo ":: ${SCRIPT_BASE}:${PID} Failure (status $status)"
-  else
-    echo ":: ${SCRIPT_BASE}:${PID} Success"
-  fi
-}
-export -f cleanup
-trap 'status=$?; cleanup; exit $status' EXIT
-trap 'trap - HUP; cleanup SIGHUP; kill -HUP $$' HUP
-trap 'trap - INT; cleanup SIGINT; kill -INT $$' INT
-trap 'trap - TERM; cleanup SIGTERM; kill -TERM $$' TERM
-#!/usr/bin/env /usr/local/bin/env-ubuntu-core
-# NOTE: OSX requires shebang to be binary file not script, this is the workaround
-# shellcheck disable=SC1090
-. "${SCRIPT_DIR}/core.source"
-
-REPO="glg/gdsservice"
-
-# Only change the above
-
-KERNEL_NAME="$(uname -s)" # Darwin/Linux
-MACHINE="$(uname -m)"
-if [[ "${MACHINE}" == "x86_64" ]]; then
-  ARCHITECTURE="amd64"
+kernel_name="$(uname -s)" # Darwin/Linux
+machine="$(uname -m)"
+if [[ "${machine}" == "x86_64" ]]; then
+  architecture="amd64"
 else
-  ARCHITECTURE="arm64"
+  architecture="arm64"
 fi
 
-# NOTE: sometimes you need ${KERNEL_NAME,,}
-read -r VERSION URL <<< "$(gh api "/repos/${REPO}/releases/latest" \
+releases="$(gh api "/repos/${repo}/releases/latest")"
+
+# NOTE: sometimes you need ${kernel_name,,}
+read -r version url <<< "$(
+  printf "%s\n" "${releases}" \
   | jq \
-  --arg k "${KERNEL_NAME}" \
-  --arg a "${ARCHITECTURE}" \
-  --arg m "${MACHINE}" \
-  -r '[.tag_name, (.assets[].browser_download_url | select(test($k+"_"+$m)))] | @tsv' \
+  --arg k "${kernel_name}" \
+  --arg a "${architecture}" \
+  --arg m "${machine}" \
+  -r '[.tag_name, (.assets[].browser_download_url | select(test($k+"_"+$m)))] | @tsv'
 )"
 
-ZIP_FILE="${URL##*/}"
-EXECUTABLE="${REPO##*/}"
-VERSIONED_EXE="${EXECUTABLE}_${VERSION}"
+info "V:${version}"
+info "U:${url}"
 
-gh release download "${VERSION}" -R "${REPO}" -p "${ZIP_FILE}"
+zip_file="${url##*/}"
+executable="${repo##*/}"
+#executable="${install_name}"
+versioned_exe="${executable}_${version}"
 
-TARGET_DIR="${HOME}/.local/${EXECUTABLE}"
-BIN_DIR="${HOME}/.local/bin"
+info "ZF:${zip_file}"
+info "E:${executable}"
+info "VE:${versioned_exe}"
 
-mkdir -p "${TARGET_DIR}"
-mkdir -p "${BIN_DIR}"
+gh release download "${version}" -R "${repo}" -p "${zip_file}"
 
-tar -xf "${ZIP_FILE}"
-#unzip "${ZIP_FILE}"
+target_dir="${HOME}/.local/${executable}"
+bin_dir="${HOME}/.local/bin"
 
-mv "${EXECUTABLE}" "${TARGET_DIR}/${VERSIONED_EXE}"
+mkdir -p "${target_dir}"
+mkdir -p "${bin_dir}"
 
-if [[ "${KERNEL_NAME}" == "Darwin" ]]; then
-  ln -sf "${TARGET_DIR}/${VERSIONED_EXE}" "${BIN_DIR}/${EXECUTABLE}"
+tar xzf "${zip_file}"
+#tar xf "${zip_file}"
+#unzip "${zip_file}"
+
+mv "${executable}" "${target_dir}/${versioned_exe}"
+
+if [[ "${kernel_name}" == "Darwin" ]]; then
+  ln -sf "${target_dir}/${versioned_exe}" "${bin_dir}/${executable}"
 else
-  ln --symbolic --force "${TARGET_DIR}/${VERSIONED_EXE}" "${BIN_DIR}/${EXECUTABLE}"
+  ln --symbolic --force "${target_dir}/${versioned_exe}" "${bin_dir}/${executable}"
 fi
-
-# vim: set ft=bash :
-#!/usr/bin/env /usr/local/bin/env-ubuntu-core
-# NOTE: OSX requires shebang to be binary file not script, this is the workaround
-# shellcheck disable=SC1090
-. "${SCRIPT_DIR}/core.source"
-
-VERSION="${1:-0.2.42}"
-TARGET="/usr/local/bin/act"
-KERNEL="$(uname -s)"
-HARDWARE="$(uname -m)"
-
-if [[ "${KERNEL}" == "Linux" ]]; then
-  ARCHITECTURE="$(dpkg --print-architecture)"
-else
-  >&2 echo ":: unsupported kernel"
-fi
-
-wget -q "https://github.com/nektos/act/releases/download/v${VERSION}/act_${KERNEL}_${HARDWARE}.tar.gz"
-tar xzf act_${KERNEL}_${HARDWARE}.tar.gz
-sudo mv "act" "${TARGET}"
-
-# vi: set ft=bash :
