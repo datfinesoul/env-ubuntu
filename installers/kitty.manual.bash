61aecb50fc96b9b4cf2a566c8f1b12d199a11768
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2023-07-08 10:55:31 +0900

    kitty editor changes

---
+++ installers/kitty.manual.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+source '_core.bash'
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	>&2 echo 'No Darwin Install'
+else
+	curl -L 'https://sw.kovidgoyal.net/kitty/installer.sh' | \
+		sh /dev/stdin launch=n
+
+	mkdir -p "${HOME}/.local/bin/"
+	ln -sf "${HOME}/.local/kitty.app/bin/kitty" "${HOME}/.local/bin/"
+	#cp "${HOME}/.local/kitty.app/share/applications/kitty.desktop" "${HOME}/.local/share/applications"
+	cat > "${HOME}/.local/share/applications/kitty.desktop" <<-DOC
+	[Desktop Entry]
+	Version=1.0
+	Type=Application
+	Name=Kitty
+	Exec=${HOME}/.local/bin/kitty
+	Terminal=false
+	Icon=${HOME}/.local/share/icons/hicolor/256x256/apps/kitty.png
+	DOC
+	rsync -avz "${HOME}/.local/kitty.app/share/icons/"* "${HOME}/.local/share/icons/"
+
+	#sed -i -e 's|Icon=kitty|Icon=/home/'"${USER}"'/.local/share/icons/hicolor/256x256/apps/kitty.png|g' \
+	#  "${HOME}/.local/share/applications/kitty.desktop"
+fi
