e92f6e59351ef3ac952b06176fb4372078e2ac94
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2024-04-22 16:08:25 +0900

    fix

--- functions/csv-to-markdown.bash
+++ functions/csv-to-markdown.bash
@@ -4,7 +4,7 @@ csv_to_markdown() {
 	  # Read the header and create the Markdown table header
     read header
     header=$(echo $header | awk -F, '{ for (i=1; i<=NF; i++) gsub(/"\\""/, "", $i); for (i=1; i<=NF; i++) printf "| %s ", $i; print "|"; }')
-    separator=$(echo $header | awk -F'|' '{ for (i=2; i<NF; i++) printf "|---"; print "|---|"; }')
+    separator=$(echo $header | awk -F'|' '{ for (i=2; i<NF; i++) printf "|---"; print "|"; }')
 
     # Output the table header and separator
     echo "$header"
