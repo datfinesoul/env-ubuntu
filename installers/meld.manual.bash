ad32f035c40d9f846d6b36624ce1bd5cca97361d
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2024-05-21 10:39:18 +0900

    updated meld installer

--- installers/meld.manual.bash
+++ installers/meld.manual.bash
@@ -1,9 +1,8 @@
-#!/usr/bin/env /usr/local/bin/env-ubuntu-core
-# NOTE: OSX requires shebang to be binary file not script, this is the workaround
-# shellcheck disable=SC1090
-. "${SCRIPT_DIR}/core.source"
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+source "$(dirname "${0}")/_core.bash"
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
+if [[ "${kernel_name}" == "Darwin" ]]; then
   true
 else
   sudo apt-get --yes update
@@ -12,7 +11,16 @@ else
     libgtk-3-dev \
     libgtksourceview-4-dev \
     python-gi-dev \
-    python3-gi-cairo
+    python3-gi-cairo \
+		python3-cairo-dev \
+		python3 \
+		python3-pip \
+		python3-setuptools \
+		python3-wheel \
+		ninja-build \
+		itstool
+
+	pip3 install --user meson
 
   git clone 'https://gitlab.gnome.org/GNOME/meld.git'
   cd meld
