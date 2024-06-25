e1bc832f9bdeb36b9c1dcc264f34eab1f606760a
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2024-06-25 17:35:36 +0900

    experimental _gs

--- functions/gs.bash
+++ functions/gs.bash
@@ -1,3 +1,31 @@
+_gs (){
+	local tld="$(git rev-parse --show-toplevel 2> /dev/null)" || tld=''
+	local command="${1}"
+	if [[ -z "${tld}" ]]; then
+		return
+	fi
+	local -a dirs
+	IFS=/ read -r -a dirs <<< "$(git rev-parse --show-prefix)"
+	local length="${#dirs[@]}"
+	local gs_path gs_commands
+	for (( i="${#dirs[@]}"; i>=0; i-- )); do
+		gs_path="${tld}$(IFS='/'; echo "/${dirs[*]:0:$i}")/gs-targets"
+		gs_path="${gs_path//\/\///}"
+		#echo $gs_path
+		if [[ -e "${gs_path}" ]]; then
+			if [[ -n "${command}" && -e "${gs_path}/${command}" ]]; then
+				"${tld}/_dev/gs-targets/${command}" "$@"
+				return
+			fi
+			gs_commands+="$(find "${gs_path}/" -type l -exec basename {} \;)\n"
+		fi
+	done
+	if [[ -z "$gs_commands" ]]; then
+		>&2 echo "[x] no gs-targets found"
+	else
+		echo "$gs_commands" | sort | >&2 uniq
+	fi
+}
 gs (){
 	local tld="$(git rev-parse --show-toplevel 2> /dev/null)" || tld=''
 	local command="${1}"
