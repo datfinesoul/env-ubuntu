540b821aec93fe30403444a3316cbb51c15de8c4
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2024-09-10 16:33:31 +0900

    finish new version of gs

--- functions/gs.bash
+++ functions/gs.bash
@@ -5,25 +5,30 @@ _gs (){
 		return
 	fi
 	local -a dirs
+	# load an array with each part of the repo based relative path
 	IFS=/ read -r -a dirs <<< "$(git rev-parse --show-prefix)"
 	local length="${#dirs[@]}"
 	local gs_path gs_commands
-	for (( i="${#dirs[@]}"; i>=0; i-- )); do
-		gs_path="${tld}$(IFS='/'; echo "/${dirs[*]:0:$i}")/gs-targets"
+	for (( i="${length}"; i>=0; i-- )); do
+		# starting with the longest path, find a _gs dir
+		gs_path="${tld}$(IFS='/'; echo "/${dirs[*]:0:$i}")/_gs"
 		gs_path="${gs_path//\/\///}"
-		#echo $gs_path
+		#echo "==$gs_path"
 		if [[ -e "${gs_path}" ]]; then
+			# if a _gs command was passed in $1 execute it
 			if [[ -n "${command}" && -e "${gs_path}/${command}" ]]; then
-				"${tld}/_dev/gs-targets/${command}" "$@"
+				"${tld}/_dev/_gs/${command}" "$@"
 				return
 			fi
-			gs_commands+="$(find "${gs_path}/" -type l -exec basename {} \;)\n"
+			# otherwise add the commands in this dir to a collection
+			gs_commands+="$(find "${gs_path}/" -type l -exec basename {} \;)"$'\n'
 		fi
 	done
 	if [[ -z "$gs_commands" ]]; then
-		>&2 echo "[x] no gs-targets found"
+		>&2 echo "[x] no _gs commands found"
 	else
-		echo "$gs_commands" | sort | >&2 uniq
+		# print the collection
+		echo -e "$gs_commands" | sort | >&2 uniq
 	fi
 }
 gs (){
