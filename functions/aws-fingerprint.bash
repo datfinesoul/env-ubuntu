40d69adbf2147d2bb92c834f090ecc22176151c5
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2024-12-09 09:12:26 +0900

    update epoch

--- functions/aws-fingerprint.bash
+++ functions/aws-fingerprint.bash
@@ -1,3 +1,4 @@
+#https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/verify-keys.html
 alias aws-fingerprint=aws_fingerprint
 aws_fingerprint () {
 	openssl pkcs8 -in "$1" -inform PEM -outform DER -topk8 -nocrypt \
