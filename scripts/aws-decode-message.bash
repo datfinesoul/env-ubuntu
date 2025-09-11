f724390e9251ee207d0754716ec2e0c3d836416b
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2025-09-11 18:15:44 +0900

    save updates

--- scripts/aws-decode-message.bash
+++ scripts/aws-decode-message.bash
@@ -2,12 +2,12 @@
 
 alias aws-decode-message=aws_decode_message
 aws_decode_message () {
-local CODE
-CODE="${1}"
-aws sts decode-authorization-message \
-	--encoded-message "${CODE}" \
-	--query DecodedMessage --output text \
-	| jq '.'
+	local CODE
+	CODE="${1}"
+	aws sts decode-authorization-message \
+		--encoded-message "${CODE}" \
+		--query DecodedMessage --output text \
+		| jq '.'
 }
 
 # Call the function with all arguments
