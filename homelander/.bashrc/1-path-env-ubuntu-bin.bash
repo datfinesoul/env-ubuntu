b422978f7f1c8235bba5ab36aa97883c1ffaf1ee
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2025-09-11 18:01:17 +0900

    move bin

--- homelander/.bashrc/1-path-env-ubuntu-bin.bash
+++ homelander/.bashrc/1-path-env-ubuntu-bin.bash
@@ -1,5 +1,5 @@
 # display the default GOPATH
 # go env GOPATH
-export PATH="${ENV_UBUNTU_ROOT}/bin:${HOME}/.local/bin:/usr/local/go/bin:${HOME}/go/bin:${PATH}"
+export PATH="${ENV_UBUNTU_ROOT}/scripts:${HOME}/.local/bin:/usr/local/go/bin:${HOME}/go/bin:${PATH}"
 export PATH="/usr/local/sbin:${PATH}"
 export PATH="/opt/homebrew/bin:${PATH}"
