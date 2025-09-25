ecb8f646765288d5b22bb47187a3b8fd17bdc572
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2025-09-25 18:24:30 +0900

    small adjustments

--- _gs/reset.bash
+++ _gs/reset.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-git reset -- "$(git rev-parse --show-toplevel)/homelander/_home/.gitconfig"
-git checkout -- "$(git rev-parse --show-toplevel)/homelander/_home/.gitconfig"
+git reset -- "$(git rev-parse --show-toplevel)/homelander/_home/.gitconfig" > /dev/null
+git checkout -- "$(git rev-parse --show-toplevel)/homelander/_home/.gitconfig" > /dev/null
