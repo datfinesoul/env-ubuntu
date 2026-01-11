47c49027719f1a6d4f345fcf7f3037596246711f
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2026-01-11 17:51:12 +0900

    lunatask icon and misc changes

--- functions/nvmrc.bash
+++ functions/nvmrc.bash
@@ -1,20 +1,25 @@
 nvmrc () {
-  # swtich to the nodejs version automatically, if an .nvmrc file is found in the current dir.
-  [[ -f .nvmrc ]] || return 0
-  local CURRENT DESIRED
-  CURRENT="$(node -v)"
-  CURRENT="${CURRENT//[$'\r\t\n v']}"
-  DESIRED="$(<.nvmrc)"
-  DESIRED="${DESIRED//[$'\r\t\n v']}"
-  if [[ -z "${CURRENT}" || -z ${DESIRED} ]]; then
-    return 0
-  fi
-  if [[ \
-    "${DESIRED}" != "${CURRENT}" \
-    && "${DESIRED}" != "${CURRENT%.*}" \
-    && "${DESIRED}" != "${CURRENT%%.*}" \
-    ]]; then
-      nvm use "${DESIRED//[$'\r\t\n v']}"
-      echo
-  fi
+	# switch to the nodejs version automatically, if an .nvmrc file is found in the current dir.
+	if ! command -v nvm &> /dev/null; then
+		return 0
+	fi
+	if [[ ! -f .nvmrc ]]; then
+		return 0
+	fi
+	local CURRENT DESIRED
+	CURRENT="$(node -v)"
+	CURRENT="${CURRENT//[$'\r\t\n v']}"
+	DESIRED="$(<.nvmrc)"
+	DESIRED="${DESIRED//[$'\r\t\n v']}"
+	if [[ -z "${CURRENT}" || -z ${DESIRED} ]]; then
+		return 0
+	fi
+	if [[ \
+		"${DESIRED}" != "${CURRENT}" \
+		&& "${DESIRED}" != "${CURRENT%.*}" \
+		&& "${DESIRED}" != "${CURRENT%%.*}" \
+		]]; then
+		nvm use "${DESIRED//[$'\r\t\n v']}"
+		echo
+	fi
 }
